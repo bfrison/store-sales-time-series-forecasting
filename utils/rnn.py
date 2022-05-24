dc4491c15fb8413577746bb7c27945dc4fd388c9
Blaise Frison <blaise.frison@gmail.com>
2022-05-23 20:04:46 -0400

    added linear layers to config

--- utils/rnn.py
+++ utils/rnn.py
@@ -2,12 +2,13 @@ import torch.nn as nn
 
 
 class Sales_RNN(nn.Module):
-    def __init__(self, input_size, hidden_dim, n_layers):
+    def __init__(self, input_size, hidden_dim, n_layers, additional_linear_layers):
         super(Sales_RNN, self).__init__()
 
         self.input_size = input_size
         self.hidden_dim = hidden_dim
         self.n_layers = n_layers
+        self.lin_layers = [hidden_dim] + additional_linear_layers + [1]
 
         self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)
 
@@ -15,9 +16,12 @@ class Sales_RNN(nn.Module):
 
         self.flatten = nn.Flatten(0, -2)
 
-        self.fc1 = nn.Linear(hidden_dim, 64)
-
-        self.fc2 = nn.Linear(64, 1)
+        self.fc = nn.Sequential(
+            *(
+                nn.Linear(self.lin_layers[i], self.lin_layers[i + 1])
+                for i in range(len(self.lin_layers) - 1)
+            )
+        )
 
     def forward(self, x, hidden):
         batch_size = x.size(0)
@@ -26,8 +30,7 @@ class Sales_RNN(nn.Module):
         r_out = self.dropout(r_out)
         r_out = self.flatten(r_out)
 
-        r_out = self.fc1(r_out)
-        r_out = self.fc2(r_out)
+        r_out = self.fc(r_out)
         output = r_out.reshape(batch_size, -1)
 
         return output, hidden
