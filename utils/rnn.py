32065f79b8d9f0ab88083ee321e11dca233895bd
Blaise Frison <blaise.frison@gmail.com>
2022-05-24 21:37:04 -0400

    added dropout after first linear layer, MSE 0.993

--- utils/rnn.py
+++ utils/rnn.py
@@ -8,7 +8,7 @@ class Sales_RNN(nn.Module):
         self.input_size = input_size
         self.hidden_dim = hidden_dim
         self.n_layers = n_layers
-        self.lin_layers = [hidden_dim] + additional_linear_layers + [1]
+        self.lin_layers = additional_linear_layers + [1]
 
         self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)
 
@@ -16,7 +16,9 @@ class Sales_RNN(nn.Module):
 
         self.flatten = nn.Flatten(0, -2)
 
-        self.fc = nn.Sequential(
+        self.fc1 = nn.Linear(hidden_dim, self.lin_layers[0])
+
+        self.fc_add = nn.Sequential(
             *(
                 nn.Linear(self.lin_layers[i], self.lin_layers[i + 1])
                 for i in range(len(self.lin_layers) - 1)
@@ -30,7 +32,9 @@ class Sales_RNN(nn.Module):
         r_out = self.dropout(r_out)
         r_out = self.flatten(r_out)
 
-        r_out = self.fc(r_out)
+        r_out = self.fc1(r_out)
+        r_out = self.dropout(r_out)
+        r_out = self.fc_add(r_out)
         output = r_out.reshape(batch_size, -1)
 
         return output, hidden
